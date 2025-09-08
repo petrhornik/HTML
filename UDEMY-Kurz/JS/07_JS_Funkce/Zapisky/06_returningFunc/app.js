98eb0d081d8acc41f589228373ba24d656798b57
Petr Horník <petrhornik2007@outlook.cz>
2025-09-08 23:59:19 +0200

    Udemy Course - more JS 23:59 8.9:25

--- UDEMY-Kurz/JS/07_JS_Funkce/Zapisky/06_returningFunc/app.js
+++ UDEMY-Kurz/JS/07_JS_Funkce/Zapisky/06_returningFunc/app.js
@@ -19,9 +19,25 @@ loterie(); //spuštění vybrané fce...
 
 // # 2 - builder funkce -> sestavování funkcí pomocí funkce WOOOOW
 
-function naleziciCislo(num){
-    return num >= 50 && num <= 100 //funkce vracející boolean dle (ne)pravdy
+// function naleziciCislo(num){
+//     return num >= 50 && num <= 100 //funkce vracející boolean dle (ne)pravdy
+// }
+
+// function naleziciCislo2(num){
+//     return num >= 0 && num <= 1 //hardcoding každé fce. FUJ!!!
+// }
+
+// const bool = naleziciCislo(87);
+// console.log(bool);
+
+function nalezCisBuilder(min, max){ //builder fce. TIME 😎
+    return function(num){
+        return num >= min && num <= max;
+    };
 }
 
-const bool = naleziciCislo(87);
+const CreatedFunc = nalezCisBuilder(30, 150); //můžu vytvořít kolik fcí chci bez samostatné deklarace
+const bool = CreatedFunc(50);
 console.log(bool);
+
+
--- UDEMY-Kurz/JS/07_JS_Funkce/Zapisky/06_returningFunc/poznamky.md
+++ UDEMY-Kurz/JS/07_JS_Funkce/Zapisky/06_returningFunc/poznamky.md
@@ -3,3 +3,5 @@
     - lze vracet fci. jako výstup např. if else rozhodováním
     - volání fce napojíme na nějakou prom. a tu následně pomocí () spustíme
       -> that's actually it...
+
+    - builder fce. 
