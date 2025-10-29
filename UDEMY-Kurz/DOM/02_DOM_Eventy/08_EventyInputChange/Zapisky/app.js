37e14066498f565aae6c120908d09945e0926c61
Petr Horník <petrhornik2007@outlook.cz>
2025-10-29 22:41:18 +0100

    initial commit

--- UDEMY-Kurz/DOM/02_DOM_Eventy/08_EventyInputChange/Zapisky/app.js
+++ UDEMY-Kurz/DOM/02_DOM_Eventy/08_EventyInputChange/Zapisky/app.js
@@ -2,10 +2,12 @@ const input = document.querySelector("input");
 const input2 = document.querySelectorAll("input")[1];
 const heading = document.querySelector("h1");
 
+/// mění se jen při opuštění inputu
 input.addEventListener("change", (evt) =>{
    console.log(input.value); // provede se př
 });
 
+/// mění se při každé změně inputu
 input2.addEventListener("input", (evt) => {
     console.log(input2.value); //provede se při každé změně inputu
     heading.innerText = input2.value;
